c0b7151cfb391f23a5d1acdea2d661d550992f1f
Ludvig <ludvig.vinoy@hotmail.com>
2024-05-23 20:40:22 +0200

    GraphQLServer Added

--- CourseProvider/Program.cs
+++ CourseProvider/Program.cs
@@ -4,49 +4,41 @@ using Infrastructure.GraphQL.ObjectTypes;
 using Infrastructure.GraphQL.Query;
 using Infrastructure.Services;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication() // Korrekt metod för ASP.NET Core Integration
-    .ConfigureServices((hostContext, services) =>
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
     {
-        // Application Insights
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
 
-        // Cosmos DB context factory
-        services.AddPooledDbContextFactory<DataContext>(options =>
+        services.AddPooledDbContextFactory<DataContext>(x =>
         {
-            var cosmosUri = hostContext.Configuration["COSMOS_URI"];
-            var cosmosDbName = hostContext.Configuration["COSMOS_DBNAME"];
-            options.UseCosmos(cosmosUri, cosmosDbName)
-                   .UseLazyLoadingProxies();
-        });
-
-        // Application services
+            x.UseCosmos(Environment.GetEnvironmentVariable("COSMOS_URI")!, Environment.GetEnvironmentVariable("COSMOS_DBNAME")!)
+            .UseLazyLoadingProxies();
+    });
+       
         services.AddScoped<ICourseService, CourseService>();
 
-        // GraphQL setup
         services.AddGraphQLFunction()
                 .AddQueryType<Query>()
                 .AddMutationType<CourseMutation>()
                 .AddType<CourseType>();
 
-        // Ensure database is created
+        services.AddGraphQLServer();
+
+
         var sp = services.BuildServiceProvider();
         using var scope = sp.CreateScope();
         var dbContextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<DataContext>>();
         using var context = dbContextFactory.CreateDbContext();
         context.Database.EnsureCreated();
-    })
-    .ConfigureLogging(logging =>
-    {
-        logging.AddConsole();
-        logging.SetMinimumLevel(LogLevel.Information);
+
+
     })
     .Build();
 
